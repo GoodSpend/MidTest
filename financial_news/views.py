5773218fd52207dca914befce06a4ec8cd1f2bc6
whysyahrizal <99377455+whysyahrizal@users.noreply.github.com>
2023-06-20 14:52:45 +0700

    Update views.py

--- financial_news/views.py
+++ financial_news/views.py
@@ -1,8 +1,12 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from .models import News
 from .forms import NewsForm
-# login required
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from financial_news.models import News
+from datetime import datetime
+from django.core import serializers
 
 @login_required(login_url='/login/')
 def news_list(request):
@@ -39,3 +43,50 @@ def news_delete(request, pk):
         news.delete()
         return redirect('financial_news:news_list')
     return render(request, 'news_confirm_delete.html', {'news': news})
+
+@csrf_exempt
+def news_list(request):
+    news = News.objects.all()
+    return HttpResponse(serializers.serialize("json", news), content_type="application/json")
+
+@csrf_exempt
+def news_create_app(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        author = request.POST['author']
+        date_str = request.POST['date']
+        date = datetime.strptime(date_str, "%d/%m/%Y").date()
+
+        news = News(title=title, content=content, author=author, publish_date=date)
+        news.save()
+        return HttpResponse(b"CREATED", status=201)
+    
+    return HttpResponseNotFound()
+
+@csrf_exempt
+def news_update_app(request):
+    news = News.objects.get(pk=int(request.POST['pk']))
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        author = request.POST['author']
+        date_str = request.POST['date']
+        
+        news.title = title
+        news.content = content
+        news.author = author
+        news.publish_date = datetime.strptime(date_str, "%d/%m/%Y").date()
+        news.save()
+        return HttpResponse(b"UPDATED", status=201)
+    
+    return HttpResponseNotFound()
+
+@csrf_exempt
+def news_delete_app(request):
+    if request.method == 'POST':
+        pk = request.POST['pk']
+        news = News.objects.get(pk=int(pk))
+        news.delete()
+        return HttpResponse(b"DELETED", status=201)
+    return HttpResponseNotFound()
