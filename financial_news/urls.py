8b942cacf4dc77cc28f23c1178b7422f615a31f4
whysyahrizal <99377455+whysyahrizal@users.noreply.github.com>
2023-06-20 14:53:11 +0700

    Update urls.py

--- financial_news/urls.py
+++ financial_news/urls.py
@@ -5,7 +5,11 @@ app_name = 'financial_news'
 
 urlpatterns = [
     path('', views.news_list, name='news_list'),
+    path('app', views.news_list, name='news_list'),
     path('news/new/', views.news_create, name='news_create'),
+    path('news/new/app', views.news_create_app, name='news_create_app'),
+    path('news/delete/app', views.news_delete_app, name='news_delete_app'),
+    path('news/edit/app', views.news_update_app, name='news_update_app'),
     path('news/<int:pk>/edit/', views.news_update, name='news_update'),
     path('news/<int:pk>/delete/', views.news_delete, name='news_delete'),
     path('login/', include('home.urls')),
